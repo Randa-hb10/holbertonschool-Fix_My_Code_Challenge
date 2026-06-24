cca32c3928e46d6c47ae8d5ba9a391df535b16f8
Randa-hb10 <rm-400000@hotmail.com>
2026-06-24 09:26:48 +0300

    add 4 delete dond

--- challenge/4-delete_dnodeint/add_dnodeint_end.c
+++ challenge/4-delete_dnodeint/add_dnodeint_end.c
@@ -12,28 +12,30 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
-	dlistint_t *l;
+    dlistint_t *new;
+    dlistint_t *last_node;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-	{
-		return (NULL);
-	}
-	new->n = n;
-	new->next = NULL;
-	if (*head == NULL)
-	{
-		*head = new;
-		new->prev = NULL;
-		return (new);
-	}
-	l = *head;
-	while (l->next != NULL)
-	{
-		l = l->next;
-	}
-	l->next = new;
-	new->prev = l;
-	return (new);
+    new = malloc(sizeof(dlistint_t));
+    if (new == NULL)
+    {
+        return (NULL);
+    }
+    new->n = n;
+    new->next = NULL;
+    if (*head == NULL)
+    {
+        *head = new;
+        new->prev = NULL;
+        return (new);
+    }
+
+    last_node = *head;
+    while (last_node->next != NULL)
+    {
+        last_node = last_node->next;
+    }
+    last_node->next = new;
+    new->prev = last_node;
+
+    return (new);
 }
